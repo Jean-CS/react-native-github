7ab12121c94d3cad8e024ef511bd5d0d77c2724e
Jean <jean.carlos@fullbar.com.br>
2019-10-09 23:56:58 -0300

    Add react-native-vector-icons and style Main component

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from './styles';
+import { Container, Form, Input, SubmitButton } from './styles';
 
 export default function Main() {
-  return <Container />;
+  return (
+    <Container>
+      <Form>
+        <Input autoCorrect={false} autoCapitalize="none" placeholder="Add user" />
+        <SubmitButton>
+          <Icon name="add" size={20} color="#fff" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 Main.navigationOptions = {
