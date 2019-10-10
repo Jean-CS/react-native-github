5326f1a3db898d57b947fc145a3903f9817af8e3
Jean <jean.carlos@fullbar.com.br>
2019-10-09 23:30:53 -0300

    Add navigation

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 import React from 'react';
-import { SafeAreaView, ScrollView, View, Text, StatusBar } from 'react-native';
 
 import './config/ReactotronConfig';
 
+import Routes from './routes';
+
 export default function App() {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView contentInsetAdjustmentBehavior="automatic">
-          <View>
-            <Text>Hello World!</Text>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
+  return <Routes />;
 }
